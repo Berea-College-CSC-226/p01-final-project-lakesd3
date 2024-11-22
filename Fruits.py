5c3ec5c81e79546b5fe0fca7c0b5bfd61b4da558
spencerc <spencerc@berea.edu>
2024-11-22 09:20:06 -0500

    Added in sprites and used T11 as a basis for reverse engineering the classes. Mostly used to test that our sprites are showing up

--- Fruits.py
+++ Fruits.py
@@ -37,7 +37,7 @@ class Fruits(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect()
         self.rect.move_ip(self.screen_size[0]//4, self.screen_size[1]//4)
         self.path = random.choice(self.directions)
-        self.position = [random.randint(-100, 100), 100]
+        self.position = [0, 0]
 
     def get_direction(self):
         """
